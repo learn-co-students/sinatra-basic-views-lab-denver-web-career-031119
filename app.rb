a57c722626d4a1144d3e2a73e1b3d51ccfc55d06
github username <github email address>
2019-04-01 13:50:11 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  # def call(environment)
+  #   status_code = 200
+  #   headers = []
+  #   body = []
+  #
+  #   [status_code, headers, body]
+  # end
 
-
+  get "/" do
+    erb :index
+  end
 end
